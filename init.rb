8bedb764de9b9993fcf742d24912e07e1214e11a
Janfy <janfy@free.fr>
2017-01-02 23:21:49 +0100

    Update version -> 2.0.1

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_flowdock do
   name 'Flowdock'
   author 'Janfy'
   description 'Notify your Flowdock flow about Redmine events'
-  version '2.0.0'
+  version '2.0.1'
   url 'https://github.com/Janfy/redmine_flowdock'
 
   Rails.configuration.to_prepare do
